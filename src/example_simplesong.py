8bb96a23ed018c1849989a0ea1ed8a7e4d8fd824
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-02-28 16:38:04 +0100

    Use CHATGPT to write to the SN76489 via I2C

---
+++ src/example_simplesong.py
@@ -0,0 +1,39 @@
+# Speelt Mary had a Little Lamb via I2C op een ESP8266 met SN76489
+# ChatGPT --> Please write a program in micropython which plays mary Had a Little Lamb using a ESP8266 and SN76489 via I2C. Also, generate a 4Mhz clock signal on pin A0.
+# https://chat.openai.com/chat/ede913fa-4fcb-44ae-81d8-b8f546e91327
+
+import machine
+import time
+
+# Define clock frequency
+clock_freq = 4000000
+
+# Set up I2C interface
+i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=clock_freq)
+
+# Set up clock signal
+clock_pin = machine.Pin(0, machine.Pin.OUT)
+clock_timer = machine.Timer(0)
+
+# Configure SN76489 for 4-bit volume, 8-bit tone registers
+i2c.writeto(0x40, b'\x9F')
+
+# Define note frequencies
+C = 0x60
+D = 0x68
+E = 0x70
+F = 0x78
+G = 0x80
+A = 0x88
+B = 0x90
+
+# Play "Mary Had a Little Lamb"
+notes = [E, D, C, D, E, E, E, D, D, D, E, G, G, E, D, C, D, E, E, E, E, D, D, E, D, C]
+durations = [4, 4, 4, 4, 4, 4, 2, 4, 4, 2, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4]
+
+# Define function to generate clock signal
+def generate_clock(timer):
+    clock_pin.value(not clock_pin.value())
+
+# Set up timer to generate clock signal
+clock_timer.init(period=int
